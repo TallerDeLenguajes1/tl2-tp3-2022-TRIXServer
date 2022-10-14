d4ecabed8b9a638106490ac037450cc094a9d63f
TRIXServer <admin@trixserver.com>
2022-10-14 15:02:35 -0300

    commit

--- classCadete.cs
+++ classCadete.cs
@@ -1,21 +1,22 @@
 public class Cadete : Persona
+{
+    private List<Pedidos> listadoPedidosCadete = new List<Pedidos>();
+
+    public List<Pedidos> ListadoPedidosCadete { get => listadoPedidosCadete; set => listadoPedidosCadete = value; }
+
+    public Cadete(string[] datosCadeteCSV)
     {
-        private List<Pedidos> listadoPedidosCadete = new List<Pedidos>();
-
-        public List<Pedidos> ListadoPedidosCadete { get => listadoPedidosCadete; set => listadoPedidosCadete = value; }
-
-        public Cadete(string[] datosCadeteCSV)
-        {
-            this.Id = Convert.ToInt32(datosCadeteCSV[0]);
-            this.Nombre = datosCadeteCSV[1];
-            this.Direccion = datosCadeteCSV[2];
-            this.Telefono = datosCadeteCSV[3];
-            this.ListadoPedidosCadete = listadoPedidosCadete;
-        }
-
-        /* public float JornalACobrar ()
-        {
-            float Jornal = ListadoPedidosCadete.Where(pedido => ListadoPedidosCadete.Pedidos.Estado == "Entregado").Sum(300);
-            return Jornal;
-        } */
+        this.Id = Convert.ToInt32(datosCadeteCSV[0]);
+        this.Nombre = datosCadeteCSV[1];
+        this.Direccion = datosCadeteCSV[2];
+        this.Telefono = datosCadeteCSV[3];
+        this.ListadoPedidosCadete = listadoPedidosCadete;
+        
     }
+
+    /* public float JornalACobrar ()
+    {
+        float Jornal = ListadoPedidosCadete.Where(pedido => ListadoPedidosCadete.Pedidos.Estado == "Entregado").Sum(300);
+        return Jornal;
+    } */
+}
